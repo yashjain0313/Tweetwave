650731c6f594395c6e02f46f8cabef0f9b5701ca
yashjain0313 <yashjain0313@gmail.com>
2025-04-01 00:47:47 +0530

    render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-          "/api": {
-            target: "http://localhost:3000",
-            changeOrigin: true,
-          }
-        }
-      }
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: true,
+  },
 });
